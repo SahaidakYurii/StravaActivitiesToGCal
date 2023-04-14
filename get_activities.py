8d1b12ae7b017c708ee730411da915887b57e172
SahaidakYurii <sahaidak.pn@ucu.edu.ua>
2023-04-14 19:13:20 +0300

    arranged code in functions

--- get_activities.py
+++ get_activities.py
@@ -10,28 +10,36 @@ secret = os.environ.get('CLIENT_SECRET')
 ref_token = os.environ.get('REF_USER_TOKEN')
 client_id = os.environ.get('CLIENT_ID')
 
-auth_token_url = "https://www.strava.com/oauth/token"
-activites_url = "https://www.strava.com/api/v3/athlete/activities"
+def refresh_auth_token():
+    """
+    uses refresh token to get auth token
+    """
+    auth_token_url = "https://www.strava.com/oauth/token"
 
-payload = {
-    'client_id': client_id,
-    'client_secret': secret,
-    'refresh_token': ref_token,
-    'grant_type': "refresh_token",
-    'f': 'json'
-}
+    payload = {
+        'client_id': client_id,
+        'client_secret': secret,
+        'refresh_token': ref_token,
+        'grant_type': "refresh_token",
+        'f': 'json'
+    }
 
-print("Requesting Token...\n")
-res = requests.post(auth_token_url, data=payload, verify=False)
-access_token = res.json()['access_token']
-print("Access Token = {}\n".format(access_token))
+    print("Requesting Token...\n")
+    res = requests.post(auth_token_url, data=payload, verify=False)
+    access_token = res.json()['access_token']
+    print("Access Token = {}\n".format(access_token))
+    return access_token
 
-header = {'Authorization': 'Bearer ' + access_token}
+def get_activities(access_token, page_num=1, per_page=30):
+    """
+    gets users activities via this users access token
+    """
+    activites_url = "https://www.strava.com/api/v3/athlete/activities"
+    header = {'Authorization': 'Bearer ' + access_token}
 
-request_page_num = 1
-all_activities = {}
+    param = {'per_page': per_page, 'page': page_num}
+    return requests.get(activites_url, headers=header, params=param).json()
 
-param = {'per_page': 10, 'page': request_page_num}
-all_activities = requests.get(activites_url, headers=header, params=param).json()
-
-pprint(all_activities)
+if __name__ == "__main__":
+    access_token = refresh_auth_token()
+    pprint(get_activities(access_token))
